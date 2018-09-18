ee71b047664a7d981bf0d5f962fa1fab549e14b1
Théo Guerin <theo.guerin.pro@gmail.com>
2018-09-18 11:51:24 +0200

    upload des pdf de test

--- tests/specs.py
+++ tests/specs.py
@@ -6,7 +6,7 @@ if modulePath not in sys.path:
 
 from ConfigEnv import Config
 import unittest
-
+import boto3
 
 from PdfSplitter import Splitter
 
@@ -15,6 +15,23 @@ class TestSplitter(unittest.TestCase):
 
     def setUp(self):
         self._config = Config(self.getCurrentPath()+"data/testConfig.json")
+        self._config.addFile = Config(self.getCurrentPath()+"data/splitterConfig.json")
+        s3 = boto3.resource(
+            's3',
+            'eu-west-1',
+            aws_access_key_id=self._config.get("AWS_ACCESS_KEY_ID"),
+            aws_secret_access_key=self._config.get("AWS_SECRET_ACCESS_KEY"),
+            aws_session_token= self._config.get("AWS_SESSION_TOKEN")
+        )
+
+        self._letterKey = self.getCurrentPath()+"data/pdf/letter.pdf";
+        self._numberKey = self.getCurrentPath()+"data/pdf/number.pdf";
+
+        bucket = s3.Bucket(self._config.get("AWS_S3_BUCKET"))
+
+        # uplaod sur le bucket de données de test
+        bucket.upload_file(self._letterKey, 'letter.pdf')
+        bucket.upload_file(self._numberKey, 'number.pdf')
 
     def getCurrentPath(self):
         return os.path.dirname(os.path.abspath(__file__))+"/"
