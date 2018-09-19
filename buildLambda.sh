a273455a7ac563217a086df55df51b5888c56336
Théo Guerin <theo.guerin.pro@gmail.com>
2018-09-19 13:06:06 +0200

    build de la lambda

---
+++ buildLambda.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+[ -e lambda.zip ] && rm lambda.zip
+
+[ -d .temp ] && rm -rf .temp
+
+mkdir .temp ;
+
+cd .temp ;
+
+cp -r ../PdfSplitter .;
+cp -r ../lambda.py .;
+
+virtualenv -p python3 .venv ;
+
+source .venv/bin/activate ;
+
+pip install -r ../requirements.txt ;
+
+mv .venv/lib/python3.6/site-packages/* . ;
+
+rm .venv -rf ;
+
+chmod 755 -R .
+
+zip -r9 ../lambda.zip * ;
+
+cd .. ;
+
+deactivate
+
+# rm -rf .temp ;
