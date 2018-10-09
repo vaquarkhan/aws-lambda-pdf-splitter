b3bbd803d52ae1ec082509a1111b31ea2a48435f
Théo Guerin <theo.guerin.pro@gmail.com>
2018-10-09 15:14:35 +0200

    ajout log

--- lambda.py
+++ lambda.py
@@ -1,7 +1,9 @@
 from PdfSplitter import Splitter
 
+import json
 
 def lambdaPdfSplitter(event, context=None):
+    print(json.dumps(event))
     spliter = Splitter()
     spliter.split(event)
     return True
