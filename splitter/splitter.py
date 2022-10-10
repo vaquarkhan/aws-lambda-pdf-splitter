752b472adcaf380ef9c6f043d665d1178eaf06e9
vvariccaNRCO <violaine.varicca@nrco.fr>
2022-10-10 11:06:11 +0200

    Mise en commentaire de la rotation

--- splitter/splitter.py
+++ splitter/splitter.py
@@ -54,11 +54,12 @@ class Splitter():
             page = infileReader.pages[i]
             cachePage.append(page)
 
-    def _getOnePage(self,fileKey,page, rotation):
+    # def _getOnePage(self,fileKey,page, rotation):
+    def _getOnePage(self,fileKey,page):
         if fileKey not in self._cachePage:
             self._cachePdfOneFile(fileKey)
         pageOut = self._cachePage[fileKey][page]
-        pageOut.Rotate = rotation
+        # pageOut.Rotate = rotation
         return pageOut
 
     def _uploadToS3(self,pdfResult,key):
@@ -77,8 +78,9 @@ class Splitter():
             for pageNumber in page["pages"]:
                 #Ajout de la page
                 #page.Rotate = 180
-                rotation = output["rotations"][i]
-                pdfResult.pages.append(self._getOnePage(key,pageNumber, rotation))
+                # rotation = output["rotations"][i]
+                # pdfResult.pages.append(self._getOnePage(key,pageNumber, rotation))
+                pdfResult.pages.append(self._getOnePage(key,pageNumber))
                 i+=1
 
         self._uploadToS3(pdfResult,output["s3Key"])
